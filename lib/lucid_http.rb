40b5d4efc47c5afa08f107a9496ab48091f02e5c
Federico Iachetti <iachetti.federico@gmail.com>
2016-12-12 19:49:15 -0300

    Moved all methods out of the main Module

--- lib/lucid_http.rb
+++ lib/lucid_http.rb
@@ -2,59 +2,60 @@ require "lucid_http/version"
 require "http"
 
 module LucidHttp
-  def response
-    @__lucid_http__res
-  end
+end
 
-  def _clean
-    instance_variables.grep(/@_lucid_http_/).each do |v|
-      remove_instance_variable(v)
-    end
-  end
+def response
+  @__lucid_http__res
+end
 
-  def _setup(url, action: :get, follow: false, form: nil, **opts)
-    _clean
-    @__lucid_http__client = HTTP.persistent("http://localhost:9292")
-    if follow
-      @__lucid_http__client = @__lucid_http__client.follow
-    end
-    @__lucid_http__path = @__lucid_http__client.default_options.persistent + url
-    @__lucid_http__res = @__lucid_http__client.send(action.to_sym, url, form: form)
+def _clean
+  instance_variables.grep(/@_lucid_http_/).each do |v|
+    remove_instance_variable(v)
   end
+end
 
-  def body
-    @__lucid_http__body ||= response.body.to_s
+def _setup(url, action: :get, follow: false, form: nil, **opts)
+  _clean
+  @__lucid_http__client = HTTP.persistent("http://localhost:9292")
+  if follow
+    @__lucid_http__client = @__lucid_http__client.follow
   end
+  @__lucid_http__path = @__lucid_http__client.default_options.persistent + url
+  @__lucid_http__res = @__lucid_http__client.send(action.to_sym, url, form: form)
+end
 
-  def status
-    @__lucid_http__status = response.status
-  end
+def body
+  @__lucid_http__body ||= response.body.to_s
+end
 
-  def content_type
-    response.content_type.mime_type
-  end
+def status
+  @__lucid_http__status = response.status
+end
 
-  def path
-    @__lucid_http__path
-  end
+def content_type
+  response.content_type.mime_type
+end
 
-  def GET(url, **opts)
-    _setup(url, **opts)
-    new_body = case status.to_i
-               when 200
-                 body
-               when 500
-                 body.each_line.first
-               else
-                 "ERROR: #{status.to_s}"
-               end
+def path
+  @__lucid_http__path
+end
 
-    # puts new_body
-    new_body
-  end
+def GET(url, **opts)
+  _setup(url, **opts)
+  new_body = case status.to_i
+             when 200
+               body
+             when 500
+               body.each_line.first
+             else
+               "ERROR: #{status.to_s}"
+             end
 
-  def POST(url, **opts)
-    _setup(url, action: :post, **opts)
-    body
-  end
+  # puts new_body
+  new_body
+end
+
+def POST(url, **opts)
+  _setup(url, action: :post, **opts)
+  body
 end
