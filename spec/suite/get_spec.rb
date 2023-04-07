8e56d1740306e5aa6a6bd54cafc85e276672ebd8
Federico Iachetti <iachetti.federico@gmail.com>
2023-04-07 07:37:07 -0300

    Named specs

--- spec/suite/get_spec.rb
+++ spec/suite/get_spec.rb
@@ -1,13 +1,12 @@
 require "lucid_http"
 
-RSpec.describe "pepe" do
+RSpec.describe "GET" do
   let(:client) { LucidHttp::Client.new(base_url: "http://localhost:9292") }
-  it "does something" do
-
+  it "can retrieve the base url" do
     expect(client.base_url).to eql("http://localhost:9292")
   end
 
-  it "does something" do
+  it "can make arequest" do
     response = client.GET("/hello")
 
     expect(response.body).to eql("<h1>Hello World!</h1>")
@@ -17,7 +16,7 @@ RSpec.describe "pepe" do
     expect(response.path).to eql("/hello")
   end
 
-  it "does something" do
+  it "can make more than one request" do
     response = client.GET("/hello")
 
     expect(response.body).to eql("<h1>Hello World!</h1>")
@@ -35,7 +34,7 @@ RSpec.describe "pepe" do
     expect(response.path).to eql("/hello/you")
   end
 
-  it "does something" do
+  it "can retourn a different status code" do
     response = client.GET("/redirect_me")
 
     expect(response.body).to eql("")
@@ -45,7 +44,7 @@ RSpec.describe "pepe" do
     expect(response.path).to eql("/redirect_me")
   end
 
-  it "does something" do
+  it "can follow redirects" do
     response = client.GET("/redirect_me", follower: LucidHttp::Follower::Follow.new)
 
     expect(response.body).to eql("You have arrived here due to a redirection.")
@@ -55,14 +54,14 @@ RSpec.describe "pepe" do
     expect(response.path).to eql("/redirect_me")
   end
 
-  it "does something" do
+  it "can get an error" do
     response = client.GET("/500")
 
     expect(response.status).to eql("500 Internal Server Error")
     expect(response.error).to eql("ArgumentError: wrong number of arguments (given 0, expected 2+) (ArgumentError)")
   end
 
-  it "does something" do
+  it "can format json responses as hashes" do
     response = client.GET("/hello_world.json", formatter: LucidHttp::Formatter::JsonFormatter.new)
 
     expected_response = {
@@ -76,5 +75,4 @@ RSpec.describe "pepe" do
     expect(response.body).to eql(expected_response)
     expect(response.content_type).to eql("application/json")
   end
-
 end
